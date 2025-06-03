37173af65c96816f75028fe8b8eb50e95180f59f
Emmanuel <emmanuel@inner-vibes.be>
2025-06-03 20:37:46 +0200

    Implement Sounds in Godot

---
+++ src/LingoEngine/Sounds/ILingoFrameworkSound.cs
@@ -0,0 +1,10 @@
+﻿namespace LingoEngine.Sounds
+{
+    public interface ILingoFrameworkSound
+    {
+        List<LingoSoundDevice> SoundDeviceList { get; }
+        int SoundLevel { get; set; }
+        bool SoundEnable { get; set; }
+        void Beep();
+    }
+}
--- src/LingoEngineGodot/GodotFactory.cs
+++ src/LingoEngineGodot/GodotFactory.cs
@@ -1,20 +1,50 @@
-﻿using ArkGodot.DirectorProxy;
-using ArkGodot.GodotLinks;
+﻿using ArkGodot.GodotLinks;
 using Godot;
 using LingoEngine;
+using LingoEngine.FrameworkCommunication;
+using LingoEngine.Sounds;
+using LingoEngineGodot.Sounds;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace LingoEngineGodot
 {
-    public class GodotFactory : ILingoFrameworkFactory
+    public class GodotFactory : ILingoFrameworkFactory, IDisposable
     {
+        private readonly List<IDisposable> _disposables = new List<IDisposable>();
+        private readonly ILingoEnvironment _environment;
         private readonly IServiceProvider _serviceProvider;
 
         public GodotFactory(ILingoEnvironment environment, IServiceProvider serviceProvider)
         {
+            _environment = environment;
             _serviceProvider = serviceProvider;
         }
+
+        #region Sound
+        public LingoMemberSound CreateMemberSound(int number, string name = "")
+        {
+            var lingoMemberSound = new LingoGodotMemberSound();
+            var memberSound = new LingoMemberSound(lingoMemberSound, number, name);
+            lingoMemberSound.Init(memberSound);
+            _disposables.Add(lingoMemberSound);
+            return memberSound;
+        }
+        public LingoSound CreateSound()
+        {
+            var lingoSound = new LingoGodotSound();
+            var soundChannel = new LingoSound(lingoSound, _environment);
+            lingoSound.Init(soundChannel);
+            return soundChannel;
+        }
+        public LingoSoundChannel CreateSoundChannel(int number)
+        {
+            var lingoSoundChannel = new LingoGodotSoundChannel(number);
+            var soundChannel = new LingoSoundChannel(lingoSoundChannel, number);
+            lingoSoundChannel.Init(soundChannel);
+            _disposables.Add(lingoSoundChannel);
+            return soundChannel;
+        } 
+        #endregion
 
 
         public T CreateSprite<T>(ILingoScore score) where T : LingoSprite
@@ -24,5 +54,10 @@ namespace LingoEngineGodot
             return lingoSprite;
         }
 
+        public void Dispose()
+        {
+            foreach (var disposable in _disposables)
+                disposable.Dispose();
+        }
     }
 }
