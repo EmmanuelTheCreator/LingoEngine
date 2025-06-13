0b61106746d25a705ca9550c89c57590eda05dd7
EmmanuelTheCreator <emmanuel@atoia.com>
2025-06-13 16:57:49 +0200

    Implement Tetris game classes

---
+++ Demo/TetriGrounds/LingoEngine.Demo.TetriGrounds.Core/MovieScripts/StarMovieScript.cs
@@ -0,0 +1,45 @@
+using LingoEngine.Movies;
+using LingoEngine.Demo.TetriGrounds.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LingoEngine.Demo.TetriGrounds.Core.MovieScripts
+{
+    // Converted from 4_StarMovie.ls
+    public class StarMovieScript : LingoMovieScript, IHasStartMovieEvent, IHasStopMovieEvent
+    {
+        private readonly GlobalVars _global;
+
+        public StarMovieScript(ILingoMovieEnvironment env, GlobalVars global) : base(env)
+        {
+            _global = global;
+        }
+
+        public void StartMovie()
+        {
+            var sp = ((LingoMovie)_Movie).GetServiceProvider();
+            var env = sp.GetRequiredService<ILingoMovieEnvironment>();
+            _global.SpriteManager = new ParentScripts.SpriteManagerParentScript(env);
+            _global.SpriteManager.Init(100);
+            _global.MousePointer = new ParentScripts.MousePointer(env);
+            _global.MousePointer.Init(0);
+        }
+
+        public void StopMovie()
+        {
+            _global.SpriteManager = null;
+            _global.MousePointer = null;
+        }
+
+        public string ReplaceSpaces(string str, int leng)
+        {
+            string thisField = str;
+            for (int i = 0; i < thisField.Length; i++)
+            {
+                if (thisField[i] == ' ')
+                    thisField = thisField.Substring(0, i) + "_" + thisField[(i + 1)..];
+            }
+            if (thisField.Length > leng) thisField = thisField.Substring(0, leng);
+            return thisField;
+        }
+    }
+}
---
+++ Demo/TetriGrounds/LingoEngine.Demo.TetriGrounds.Core/Sprites/Behaviors/AppliBgBehavior.cs
@@ -0,0 +1,46 @@
+using LingoEngine.Events;
+using LingoEngine.Movies;
+
+namespace LingoEngine.Demo.TetriGrounds.Core.Sprites.Behaviors
+{
+    // Converted from 16_AppliBg.ls
+    public class AppliBgBehavior : LingoSpriteBehavior, IHasBeginSpriteEvent, IHasExitFrameEvent, IHasEndSpriteEvent
+    {
+        private bool myCheckStartData;
+        private bool myStop;
+        private int myStartLines;
+        private int myStartLevel;
+        public AppliBgBehavior(ILingoMovieEnvironment env) : base(env){}
+
+        public void BeginSprite()
+        {
+            myCheckStartData = true;
+        }
+
+        public void DataLoaded(string data, object obj){}
+        public void SendData(string type, object data){}
+
+        public void ExitFrame()
+        {
+            if (myCheckStartData)
+            {
+                if (myStop)
+                    _Movie.GoTo(_Movie.CurrentFrame);
+                else
+                    _Movie.GoTo("Game");
+            }
+        }
+
+        public void GameFinished(int score){}
+        public void ReturnFromSaveScore(string data){}
+        public void PersonalHighscores(){}
+        public void ShowGeneralScores(){}
+        public void RefeshHighScores(){}
+        public void NewText(string text){}
+        public void TextFinished(object obj){}
+        public void DestroyOverscreenTxt(){}
+        public int GetCounterStartData(string type) => 0;
+
+        public void EndSprite(){}
+    }
+}
