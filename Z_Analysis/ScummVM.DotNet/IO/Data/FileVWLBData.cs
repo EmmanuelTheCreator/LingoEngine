0d3b81e89f7c1c76c0f8104d02c08a0360c8b5b8
EmmanuelTheCreator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-06-13 09:18:44 +0200

    Add cast member raw data support

--- Z_Analysis/ScummVM.DotNet/IO/Data/FileVWLBData.cs
+++ Z_Analysis/ScummVM.DotNet/IO/Data/FileVWLBData.cs
@@ -5,10 +5,14 @@
     /// </summary>
     public class FileVWLBData
     {
+        /// <summary>List of frame labels defined in the movie.</summary>
         public List<LabelTimeLine> Labels { get; set; } = new();
+
         public class LabelTimeLine
         {
+            /// <summary>Frame number at which the label appears.</summary>
             public int Frame { get; set; }
+            /// <summary>Text of the label.</summary>
             public string Label { get; set; } = "";
         }
     }
