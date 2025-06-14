bcccefd2c8b5c5aa4b0dea5d95a9229320b1a2fe
EmmanuelTheCreator <emmanuel@atoia.com>
2025-06-14 20:37:12 +0200

    Add DirUtil xlib stub and expand DirectorEngine

---
+++ Z_Analysis/ScummVM.DotNet/XLibs/DirUtilXObj.cs
@@ -0,0 +1,32 @@
+namespace Director.XLibs
+{
+    public static class DirUtilXObj
+    {
+        public const string XlibName = "DirUtil";
+
+        public static void Open()
+        {
+            // TODO: expose xlib to Lingo runtime
+        }
+
+        public static void Close()
+        {
+            // TODO: unexpose xlib from Lingo runtime
+        }
+
+        public static void New(int nargs) { }
+        public static void Dispose(int nargs) { }
+        public static void Name(int nargs) { }
+        public static void Status(int nargs) { }
+        public static void Error(int nargs) { }
+        public static void LastError(int nargs) { }
+        public static void SetAttrib(int nargs) { }
+        public static void GetAttrib(int nargs) { }
+        public static void GetDriveType(int nargs) { }
+        public static void QTVersion(int nargs) { }
+        public static void QTVersionStr(int nargs) { }
+        public static void IsCdrom(int nargs) { }
+        public static void Authors(int nargs) { }
+        public static void SetErrorMode(int nargs) { }
+    }
+}
