0483daa2cd29852d9e2747aed52d3fc52a835100
EmmanuelTheCreator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-06-16 04:00:46 +0200

    Fix director windows

--- src/Director/LingoEngine.Director.LGodot/Movies/DirGodotStageWindow.cs
+++ src/Director/LingoEngine.Director.LGodot/Movies/DirGodotStageWindow.cs
@@ -7,6 +7,8 @@ namespace LingoEngine.Director.LGodot.Movies;
 
 internal partial class DirGodotStageWindow : BaseGodotWindow, ILingoFrameworkStageWindow, ILingoFrameworkStage
 {
+    private readonly Button _rewindButton = new Button();
+    private readonly Button _playButton = new Button();
     private bool _toggleKey;
     private LingoMovie? _movie;
     private ILingoFrameworkStage? _stage;
@@ -18,6 +20,37 @@ internal partial class DirGodotStageWindow : BaseGodotWindow, ILingoFrameworkSta
         Size = new Vector2(640, 480);
         CustomMinimumSize = Size;
         root.AddChild(this);
+
+        _rewindButton.Position = new Vector2(3, 2);
+        _rewindButton.CustomMinimumSize = new Vector2(20, 16);
+        _rewindButton.Text = "<<";
+        AddChild(_rewindButton);
+
+        _playButton.Position = new Vector2(26, 2);
+        _playButton.CustomMinimumSize = new Vector2(40, 16);
+        _playButton.Text = "Play";
+        AddChild(_playButton);
+
+        _rewindButton.Pressed += () => _movie?.GoTo(1);
+        _playButton.Pressed += OnPlayPressed;
+    }
+
+    private void OnPlayPressed()
+    {
+        if (_movie == null) return;
+        if (_movie.IsPlaying)
+            _movie.Halt();
+        else
+            _movie.Play();
+        UpdatePlayButton();
+    }
+
+    private void UpdatePlayButton()
+    {
+        if (_movie == null)
+            _playButton.Text = "Play";
+        else
+            _playButton.Text = _movie.IsPlaying ? "Stop" : "Play";
     }
 
 
