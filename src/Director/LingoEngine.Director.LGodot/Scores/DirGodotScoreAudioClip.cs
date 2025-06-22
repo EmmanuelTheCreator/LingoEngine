69ec79c758ea8e110561b036b8d8e1cd1d6c2505
EmmanuelTheCreator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-06-22 05:22:17 +0200

    Add audio tracks to score

---
+++ src/Director/LingoEngine.Director.LGodot/Scores/DirGodotScoreAudioClip.cs
@@ -0,0 +1,20 @@
+using Godot;
+using LingoEngine.Movies;
+
+namespace LingoEngine.Director.LGodot.Scores;
+
+internal class DirGodotScoreAudioClip
+{
+    internal readonly LingoAudioClip Clip;
+    internal DirGodotScoreAudioClip(LingoAudioClip clip)
+    {
+        Clip = clip;
+    }
+
+    internal void Draw(CanvasItem canvas, Vector2 position, float width, float height, Font font)
+    {
+        canvas.DrawRect(new Rect2(position.X, position.Y, width, height), new Color("#ccffcc"));
+        canvas.DrawString(font, new Vector2(position.X + 4, position.Y + font.GetAscent() - 6),
+            Clip.Sound.Name ?? string.Empty, HorizontalAlignment.Left, width - 4, 9, Colors.Black);
+    }
+}
