fbd184908e4ff7a0b116a609272ac67c0ab0f80b
EmmanuelTheCreator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-06-24 07:49:20 +0200

    Add margin support to wrap panels

--- src/LingoEngine.LGodot/Gfx/LingoGodotWrapPanel.cs
+++ src/LingoEngine.LGodot/Gfx/LingoGodotWrapPanel.cs
@@ -1,5 +1,6 @@
 using Godot;
 using LingoEngine.Gfx;
+using LingoEngine.Primitives;
 using System;
 using System.Linq;
 
@@ -12,10 +13,14 @@ namespace LingoEngine.LGodot.Gfx
     {
         private FlowContainer _container;
         private LingoOrientation _orientation;
+        private LingoMargin _itemMargin;
+        private LingoMargin _margin;
 
         public LingoGodotWrapPanel(LingoWrapPanel panel, LingoOrientation orientation)
         {
             _orientation = orientation;
+            _itemMargin = LingoMargin.Zero;
+            _margin = LingoMargin.Zero;
             _container = orientation == LingoOrientation.Horizontal ? new HFlowContainer() : new VFlowContainer();
             AddChild(_container);
             panel.Init(this);
@@ -36,17 +41,60 @@ namespace LingoEngine.LGodot.Gfx
                 _orientation = value;
                 _container = value == LingoOrientation.Horizontal ? new HFlowContainer() : new VFlowContainer();
                 AddChild(_container);
+                ApplyMargin();
                 foreach (var c in children)
+                {
                     _container.AddChild(c);
+                    if (c is Control ctrl)
+                        ApplyItemMargin(ctrl);
+                }
+            }
+        }
+
+        public LingoMargin ItemMargin
+        {
+            get => _itemMargin;
+            set
+            {
+                _itemMargin = value;
+                foreach (var child in _container.GetChildren().OfType<Control>())
+                    ApplyItemMargin(child);
+            }
+        }
+
+        public LingoMargin Margin
+        {
+            get => _margin;
+            set
+            {
+                _margin = value;
+                ApplyMargin();
             }
         }
 
         public void AddChild(ILingoFrameworkWrapPanel child)
         {
             var node = (LingoGodotWrapPanel)child;
+            ApplyItemMargin(node);
             _container.AddChild(node);
         }
 
         public void Dispose() => QueueFree();
+
+        private void ApplyItemMargin(Control ctrl)
+        {
+            ctrl.AddThemeConstantOverride("margin_left", (int)_itemMargin.Left);
+            ctrl.AddThemeConstantOverride("margin_right", (int)_itemMargin.Right);
+            ctrl.AddThemeConstantOverride("margin_top", (int)_itemMargin.Top);
+            ctrl.AddThemeConstantOverride("margin_bottom", (int)_itemMargin.Bottom);
+        }
+
+        private void ApplyMargin()
+        {
+            _container.AddThemeConstantOverride("margin_left", (int)_margin.Left);
+            _container.AddThemeConstantOverride("margin_right", (int)_margin.Right);
+            _container.AddThemeConstantOverride("margin_top", (int)_margin.Top);
+            _container.AddThemeConstantOverride("margin_bottom", (int)_margin.Bottom);
+        }
     }
 }
