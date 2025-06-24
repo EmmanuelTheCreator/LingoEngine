1e41cf76d12b7faaa11d42f1845f7950043766e5
EmmanuelTheCreator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-06-24 07:49:12 +0200

    Add wrap panel interface and Godot implementation

---
+++ src/LingoEngine/Gfx/LingoWrapPanel.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace LingoEngine.Gfx
+{
+    /// <summary>
+    /// Engine level wrapper for a panel that arranges children with wrapping.
+    /// </summary>
+    public class LingoWrapPanel : IDisposable
+    {
+#pragma warning disable CS8618
+        private ILingoFrameworkWrapPanel _framework;
+#pragma warning restore CS8618
+
+        /// <summary>Initialize with the framework specific panel.</summary>
+        public void Init(ILingoFrameworkWrapPanel framework) => _framework = framework;
+
+        public T Framework<T>() where T : ILingoFrameworkWrapPanel => (T)_framework;
+
+        public LingoOrientation Orientation
+        {
+            get => _framework.Orientation;
+            set => _framework.Orientation = value;
+        }
+
+        public void AddChild(LingoWrapPanel panel) => _framework.AddChild(panel.Framework<ILingoFrameworkWrapPanel>());
+
+        public void Dispose() => (_framework as IDisposable)?.Dispose();
+    }
+}
