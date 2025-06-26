6f1f43c8d42167b0da77d8c6381892eb3c4dd320
EmmanuelTheCreator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-06-26 17:08:13 +0200

    Add GFX menu system

---
+++ src/LingoEngine/Gfx/LingoMenuItem.cs
@@ -0,0 +1,22 @@
+namespace LingoEngine.Gfx
+{
+    /// <summary>
+    /// Engine level wrapper for a single menu item.
+    /// </summary>
+    public class LingoMenuItem
+    {
+        private ILingoFrameworkGfxMenuItem _framework = null!;
+        internal void Init(ILingoFrameworkGfxMenuItem framework) => _framework = framework;
+        internal ILingoFrameworkGfxMenuItem Framework => _framework;
+
+        public string Name { get => _framework.Name; set => _framework.Name = value; }
+        public bool Enabled { get => _framework.Enabled; set => _framework.Enabled = value; }
+        public bool CheckMark { get => _framework.CheckMark; set => _framework.CheckMark = value; }
+        public string? Shortcut { get => _framework.Shortcut; set => _framework.Shortcut = value; }
+        public event System.Action? Activated
+        {
+            add { _framework.Activated += value; }
+            remove { _framework.Activated -= value; }
+        }
+    }
+}
