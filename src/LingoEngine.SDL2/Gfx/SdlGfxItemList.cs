c6e3626c4285c301159926d4c7bb24349f0f5523
EmmanuelTheCreator <214685372+EmmanuelTheCreator@users.noreply.github.com>
2025-06-30 20:22:17 +0200

    Implement key-value ItemList

---
+++ src/LingoEngine.SDL2/Gfx/SdlGfxItemList.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using LingoEngine.Gfx;
+using LingoEngine.Primitives;
+
+namespace LingoEngine.SDL2.Gfx
+{
+    internal class SdlGfxItemList : ILingoFrameworkGfxItemList, IDisposable
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public bool Visibility { get; set; } = true;
+        public string Name { get; set; } = string.Empty;
+        public bool Enabled { get; set; } = true;
+        public LingoMargin Margin { get; set; } = LingoMargin.Zero;
+
+        private readonly List<KeyValuePair<string,string>> _items = new();
+        public IReadOnlyList<KeyValuePair<string,string>> Items => _items;
+        public int SelectedIndex { get; set; } = -1;
+        public string? SelectedKey { get; set; }
+        public string? SelectedValue { get; set; }
+
+        public event Action? ValueChanged;
+        public object FrameworkNode => this;
+        public void AddItem(string key, string value)
+        {
+            _items.Add(new KeyValuePair<string,string>(key, value));
+        }
+        public void ClearItems()
+        {
+            _items.Clear();
+            SelectedIndex = -1;
+            SelectedKey = null;
+            SelectedValue = null;
+        }
+        public void Dispose() { }
+    }
+}
