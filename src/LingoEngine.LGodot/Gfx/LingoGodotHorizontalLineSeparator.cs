112ffbeff1115d75d5db702d857cb04a97022032
Emmanuel <emmanuel@atoia.com>
2025-06-30 16:32:31 +0200

    ADd hLine, VLine

---
+++ src/LingoEngine.LGodot/Gfx/LingoGodotHorizontalLineSeparator.cs
@@ -0,0 +1,29 @@
+﻿using Godot;
+
+namespace LingoEngine.LGodot.Gfx
+{
+    public partial class LingoGodotHorizontalLineSeparator : Control
+    {
+        public override void _Ready()
+        {
+            CustomMinimumSize = new Vector2(0, 2); // 2px tall
+        }
+
+        public override void _Draw()
+        {
+            // Top: light gray
+            DrawLine(new Vector2(0, 0), new Vector2(Size.X, 0), new Color(1, 1, 1), 1);
+            // Bottom: dark gray
+            DrawLine(new Vector2(0, 1), new Vector2(Size.X, 1), new Color(0.4f, 0.4f, 0.4f), 1);
+
+        }
+
+        public override void _Notification(int what)
+        {
+            if (what == NotificationResized)
+            {
+                QueueRedraw();
+            }
+        }
+    }
+}
---
+++ src/LingoEngine.LGodot/Gfx/LingoGodotVerticalLineSeparator.cs
@@ -0,0 +1,27 @@
+﻿using Godot;
+
+namespace LingoEngine.LGodot.Gfx
+{
+    public partial class LingoGodotVerticalLineSeparator : Control
+    {
+        public override void _Ready()
+        {
+            CustomMinimumSize = new Vector2(2, 0); // or (2, 0) for vertical
+        }
+
+        public override void _Draw()
+        {
+            DrawLine(new Vector2(0, 0), new Vector2(0, Size.Y), new Color(1, 1, 1), 1); // left light
+            DrawLine(new Vector2(1, 0), new Vector2(1, Size.Y), new Color(0.4f, 0.4f, 0.4f), 1); // right dark
+
+        }
+
+        public override void _Notification(int what)
+        {
+            if (what == NotificationResized)
+            {
+                QueueRedraw();
+            }
+        }
+    }
+}
