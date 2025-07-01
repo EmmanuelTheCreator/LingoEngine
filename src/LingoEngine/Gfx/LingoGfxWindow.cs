a9be708d16f602b83e0f301e1694326b6985d74d
EmmanuelTheCreator <emmanuel@atoia.com>
2025-07-01 12:59:48 +0200

    Add window events

--- src/LingoEngine/Gfx/LingoGfxWindow.cs
+++ src/LingoEngine/Gfx/LingoGfxWindow.cs
@@ -1,4 +1,5 @@
 using LingoEngine.Primitives;
+using System;
 
 namespace LingoEngine.Gfx
 {
@@ -10,6 +11,22 @@ namespace LingoEngine.Gfx
         public string Title { get => _framework.Title; set => _framework.Title = value; }
         public LingoColor BackgroundColor { get => _framework.BackgroundColor; set => _framework.BackgroundColor = value; }
         public bool IsPopup { get => _framework.IsPopup; set => _framework.IsPopup = value; }
+
+        public event Action? OnOpen
+        {
+            add { _framework.OnOpen += value; }
+            remove { _framework.OnOpen -= value; }
+        }
+        public event Action? OnClose
+        {
+            add { _framework.OnClose += value; }
+            remove { _framework.OnClose -= value; }
+        }
+        public event Action<float, float>? OnResize
+        {
+            add { _framework.OnResize += value; }
+            remove { _framework.OnResize -= value; }
+        }
 
         public LingoGfxWindow AddItem(ILingoGfxNode node)
         {
